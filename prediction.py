6f94569b8ddef631391db332b5c4375ad4c058f9
ruslan <ruslan1123@gmail.com>
2020-10-07 19:08:08 +0300

    feat: Add device and paths arguments for feature extractor

--- prediction.py
+++ prediction.py
@@ -27,12 +27,9 @@ import captioning.models
 
 
 class FeatureExtractor:
-    TARGET_IMAGE_SIZE = [448, 448]
-    CHANNEL_MEAN = [0.485, 0.456, 0.406]
-    CHANNEL_STD = [0.229, 0.224, 0.225]
-
-    def __init__(self):
-        self.detection_model = self._build_detection_model()
+    def __init__(self, checkpoint_path, config_path, device="cpu"):
+        self.device = torch.device(device)
+        self.detection_model = self._build_detection_model(checkpoint_path, config_path)
 
     def __call__(self, url):
         with torch.no_grad():
@@ -40,32 +37,21 @@ class FeatureExtractor:
 
         return detectron_features
 
-    def _build_detection_model(self):
-        cfg.merge_from_file('model_data/detectron_model.yaml')
+    def _build_detection_model(self, checkpoint_path, config_path):
+        cfg.merge_from_file(config_path)
         cfg.freeze()
 
         model = build_detection_model(cfg)
-        checkpoint = torch.load('model_data/detectron_model.pth',
-                                map_location=torch.device("cpu"))
+        checkpoint = torch.load(checkpoint_path, map_location=torch.device("cpu"))
 
         load_state_dict(model, checkpoint.pop("model"))
 
-        model.to("cuda")
+        model.to(self.device)
         model.eval()
         return model
 
-    def get_actual_image(self, image_path):
-        if image_path.startswith('http'):
-            path = requests.get(image_path, stream=True).raw
-        else:
-            path = image_path
-
-        return path
-
     def _image_transform(self, image_path):
-        path = self.get_actual_image(image_path)
-
-        img = Image.open(path).convert("RGB")
+        img = Image.open(image_path).convert("RGB")
         im = np.array(img).astype(np.float32)
         im = im[:, :, ::-1]
         im -= np.array([102.9801, 115.9465, 122.7717])
@@ -77,20 +63,12 @@ class FeatureExtractor:
         if np.round(im_scale * im_size_max) > 1333:
             im_scale = float(1333) / float(im_size_max)
         im = cv2.resize(
-            im,
-            None,
-            None,
-            fx=im_scale,
-            fy=im_scale,
-            interpolation=cv2.INTER_LINEAR
+            im, None, None, fx=im_scale, fy=im_scale, interpolation=cv2.INTER_LINEAR
         )
         img = torch.from_numpy(im).permute(2, 0, 1)
         return img, im_scale
 
-    def _process_feature_extraction(self, output,
-                                    im_scales,
-                                    feat_name='fc6',
-                                    conf_thresh=0.2):
+    def _process_feature_extraction(self, output, im_scales, feat_name="fc6"):
         batch_size = len(output[0]["proposals"])
         n_boxes_per_image = [len(_) for _ in output[0]["proposals"]]
         score_list = output[0]["scores"].split(n_boxes_per_image)
@@ -109,9 +87,9 @@ class FeatureExtractor:
             for cls_ind in range(1, scores.shape[1]):
                 cls_scores = scores[:, cls_ind]
                 keep = nms(dets, cls_scores, 0.5)
-                max_conf[keep] = torch.where(cls_scores[keep] > max_conf[keep],
-                                             cls_scores[keep],
-                                             max_conf[keep])
+                max_conf[keep] = torch.where(
+                    cls_scores[keep] > max_conf[keep], cls_scores[keep], max_conf[keep]
+                )
 
             keep_boxes = torch.argsort(max_conf, descending=True)[:100]
             feat_list.append(feats[i][keep_boxes])
@@ -121,33 +99,40 @@ class FeatureExtractor:
         im, im_scale = self._image_transform(image_path)
         img_tensor, im_scales = [im], [im_scale]
         current_img_list = to_image_list(img_tensor, size_divisible=32)
-        current_img_list = current_img_list.to('cuda')
+        current_img_list = current_img_list.to(self.device)
         with torch.no_grad():
             output = self.detection_model(current_img_list)
-        feat_list = self._process_feature_extraction(output, im_scales,
-                                                     'fc6', 0.2)
+        feat_list = self._process_feature_extraction(output, im_scales, "fc6")
         return feat_list[0]
 
 
 if __name__ == "__main__":
-    feature_extractor = FeatureExtractor()
-    infos = captioning.utils.misc.pickle_load(open('model_data/infos_trans12-best.pkl', 'rb'))
+    device = "cpu"
+    feature_extractor = FeatureExtractor(
+        checkpoint_path="model_data/detectron_model.pth",
+        config_path="model_data/detectron_model.yaml",
+        device=device,
+    )
 
-    infos['opt'].vocab = infos['vocab']
+    infos = captioning.utils.misc.pickle_load(
+        open("model_data/infos_trans12-best.pkl", "rb")
+    )
+    infos["opt"].vocab = infos["vocab"]
 
-    model = captioning.models.setup(infos['opt'])
-    model.cuda()
-    model.load_state_dict(torch.load('model_data/model-best.pth'))
-
+    model = captioning.models.setup(infos["opt"])
+    model.to(device)
+    model.load_state_dict(torch.load("model_data/model-best.pth"))
 
     def get_captions(img_feature):
         # Return the 5 captions from beam serach with beam size 5
-        return model.decode_sequence(model(img_feature.mean(0)[None], img_feature[None], mode='sample',
-                                           opt={'beam_size': 5, 'sample_method': 'beam_search', 'sample_n': 5})[0])
+        return model.decode_sequence(
+            model(
+                img_feature.mean(0)[None],
+                img_feature[None],
+                mode="sample",
+                opt={"beam_size": 5, "sample_method": "beam_search", "sample_n": 5},
+            )[0]
+        )
 
-
-    image_path = feature_extractor.get_actual_image('test.jpg')
-    image = Image.open(image_path)
-
-    captions = get_captions(feature_extractor(image_path))
+    captions = get_captions(feature_extractor('test.png'))
     print(captions)
