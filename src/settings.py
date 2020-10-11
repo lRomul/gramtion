c0a8e3a6bebdb86e867ee493098d20afed67d398
ruslan <ruslan1123@gmail.com>
2020-10-11 21:01:03 +0300

    feat: Load settings from .env file

---
+++ src/settings.py
@@ -0,0 +1,15 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    consumer_key: str
+    consumer_secret: str
+    access_token: str
+    access_token_secret: str
+
+    class Config:
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
+
+
+settings = Settings()
