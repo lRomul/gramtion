5a400a9a39d57bc6767e96baabe53f3384cf0a4f
Ruslan Baikulov <ruslan1123@gmail.com>
2021-04-21 19:21:41 +0300

    CaptionPredictor based on virtex captioning. Unfortunately it works much worse :(

--- src/image_captioning.py
+++ src/image_captioning.py
@@ -1,9 +1,11 @@
 import torch
+import numpy as np
 from PIL import Image
 from typing import List
 
-import virtex
 from virtex import model_zoo
+from virtex.config import Config
+from virtex.factories import TokenizerFactory
 from virtex.data.transforms import DEFAULT_IMAGE_TRANSFORM
 
 from src.pydantic_models import Caption
@@ -21,9 +23,23 @@ class CaptionPredictor:
         self.model = model_zoo.get(config_path, pretrained=True)
         self.model = self.model.to(device=self.device)
 
+        config = Config("/virtex/configs/" + config_path)
+        self.tokenizer = TokenizerFactory.from_config(config)
+
     def get_captions(self, image: Image) -> List[Caption]:
-        pass
-        return []
+        image = np.array(image)
+        image = DEFAULT_IMAGE_TRANSFORM(image=image)["image"]
+        image = np.transpose(image, (2, 0, 1))[np.newaxis]
+
+        image = torch.tensor(image, dtype=torch.float, device=self.device)
+
+        with torch.no_grad():
+            output_dict = self.model({"image": image})
+
+        caption = output_dict["predictions"][0]
+        caption = self.tokenizer.decode(caption.tolist())
+        caption = Caption(text=caption)
+        return [caption]
 
     def __repr__(self):
         return generate_repr(
